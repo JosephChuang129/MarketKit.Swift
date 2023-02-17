4832db6f9e76ab4263697937dcaef7110a063cb4
joseph <joseph_chaung@owlting.com>
2023-02-17 16:47:40 +0800

    add USDC testnet

--- Sources/MarketKit/Classes/Storage/CoinStorage.swift
+++ Sources/MarketKit/Classes/Storage/CoinStorage.swift
@@ -226,6 +226,10 @@ extension CoinStorage {
             for tokenRecord in tokenRecords {
                 try? tokenRecord.insert(db)
             }
+            
+            try? TokenRecord(coinUid: "usd-coin", blockchainUid: "ethereum", type: "eip20", decimals: 6, reference: "0x07865c6E87B9F70255377e024ace6630C1Eaa37F").insert(db)
+            try? TokenRecord(coinUid: "usd-coin", blockchainUid: "polygon-pos", type: "eip20", decimals: 6, reference: "0x0FA8781a83E46826621b3BC094Ea2A0212e71B23").insert(db)
+            try? TokenRecord(coinUid: "usd-coin", blockchainUid: "avalanche", type: "eip20", decimals: 6, reference: "0x5425890298aed601595a70AB815c96711a31Bc65").insert(db)
         }
     }
 
